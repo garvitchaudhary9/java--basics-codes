38d9ee5efa3e1b4aee17d5f234953d7bd2dd78c0
chaudharygarvit9 <garvit.agdav@gmail.com>
2020-05-20 16:16:26 +0530

    Add files via upload

---
+++ Input.java
@@ -0,0 +1,16 @@
+package introduction;
+import java.util.Scanner;
+public class Input {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner x= new Scanner(System.in);
+		
+		System.out.println("enter username");
+		String userName=x.nextLine();
+		System.out.println("username :"+userName);
+		
+
+	}
+
+}
