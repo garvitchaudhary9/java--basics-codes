e0ea9eb794965d33007f2c1d1ca471f4dba9a6a7
chaudharygarvit9 <garvit.agdav@gmail.com>
2020-05-23 14:51:02 +0530

    Add files via upload

---
+++ doWhileLoop.java
@@ -0,0 +1,15 @@
+package whileLoop;
+import java.util.Scanner;
+
+public class doWhileLoop {
+
+	public static void main(String[] args) {
+		int x=1;
+		Scanner sc = new Scanner(System.in);
+		do {
+			x=sc.nextInt();
+		}while(x!=0);
+
+	}
+
+}
