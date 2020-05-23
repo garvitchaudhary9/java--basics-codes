182991c37ea4c67ae00d1ee2498879359c63ead9
chaudharygarvit9 <garvit.agdav@gmail.com>
2020-05-23 21:01:46 +0530

    Add files via upload

---
+++ matrixAddition.java
@@ -0,0 +1,48 @@
+package arrays;
+
+import java.util.Scanner;
+
+public class matrixAddition {
+
+	public static void main(String[] args) {
+		Scanner sc= new Scanner(System.in);
+		int r,c;
+		System.out.println("enter the no of rows and columns respectively");
+		r= sc.nextInt();
+		c=sc.nextInt();
+	 int a[][]= new int [r][c];
+	 int b[][]= new int [r][c];
+	 int d[][]= new int [r][c];
+	 System.out.println("Enter the array a:");
+	 for(int i =0; i<r;i++)
+	 {
+		 for(int j=0;j<c;j++) {
+			 a[i][j]=sc.nextInt();
+		 }
+	 }
+	 System.out.println("Enter the array b:");
+	 for(int i =0; i<r;i++)
+	 {
+		 for(int j=0;j<c;j++) {
+			 b[i][j]=sc.nextInt();
+		 }
+	 }
+	 
+	 for(int i =0; i<r;i++)
+	 {
+		 for(int j=0;j<c;j++) {
+			 d[i][j]=a[i][j]+b[i][j];
+		 }
+	 }
+	 System.out.println("the sum of the array a and b is c :");
+	 for(int i =0; i<r;i++)
+	 {
+		 for(int j=0;j <  c;j++) {
+			 System.out.println(d[i][j]+" ");
+		 }
+		 
+	 }
+
+	}
+
+}
